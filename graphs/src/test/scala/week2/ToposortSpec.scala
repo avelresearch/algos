c9a634db0659080512312cee3ea3132e0b4ff0a2
Pavel <pavel.oliynyk@gmail.com>
2018-09-08 20:44:09 +0100

    Refactor TopoSortSpec

--- graphs/src/test/scala/week2/ToposortSpec.scala
+++ graphs/src/test/scala/week2/ToposortSpec.scala
@@ -2,8 +2,28 @@ import org.scalatest.FlatSpec
 import scala.collection.mutable.ListBuffer
 
 class ToposortSpec extends FlatSpec {
-  "Directed graph" should "be consistent" in {
-    val _adj =
+
+  trait Fixture {
+    def toGraph(str: String) : Array[List[Int]] = {
+      // String to Array
+      val data = str.split("\n")
+        .map(_.trim())
+        .filter(_.nonEmpty)
+        .map(_.split(" "))
+        .map(a => a.map(_.toInt))
+
+      // Array to Array[List[Int]]
+      val adj: Array[ListBuffer[Int]] = Array.fill(5)(ListBuffer[Int]())
+      data.foreach(p => {
+        val (x, y) = (p(0), p(1))
+        adj(x - 1).append(y - 1)
+      })
+      adj.map(_.toList)
+    }
+  }
+
+  "Directed graph" should "have next topoligical order 5, 4, 3, 2, 1" in new Fixture {
+    val str =
       """
         |2 1
         |3 2
@@ -13,19 +33,9 @@ class ToposortSpec extends FlatSpec {
         |5 2
         |5 3
       """.stripMargin
-        .split("\n")
-        .map(_.trim())
-        .filter(_.nonEmpty)
-        .map(_.split(" "))
-        .map(a => a.map(_.toInt))
+    val graph = toGraph(str)
 
-    val adj: Array[ListBuffer[Int]] = Array.fill(5)(ListBuffer[Int]())
-    _adj.foreach(p => {
-      val (x, y) = (p(0), p(1))
-      adj(x - 1).append(y - 1)
-    })
-
-    val testable = new Toposort(5, 7, adj.map(_.toList))
+    val testable = new Toposort(5, 7, graph)
     testable.run()
     assert(testable.order.reverse == List(5, 4, 3, 2, 1), "Must be 5, 4, 3, 2, 1")
   }
