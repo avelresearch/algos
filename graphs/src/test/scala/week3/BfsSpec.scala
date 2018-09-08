b54a9f48cafd8f5d3e6b17dcd16d39cb07d258ef
Pavel <pavel.oliynyk@gmail.com>
2018-09-08 21:08:07 +0100

    Add BfsSpec

---
+++ graphs/src/test/scala/week3/BfsSpec.scala
@@ -0,0 +1,19 @@
+import org.scalatest.FlatSpec
+
+class BfsSpec extends FlatSpec {
+
+  //TODO: add test coverage for Bfs task
+//  "Directed graph" should "be consistent" in {
+//    val graph: Array[List[Int]] =
+//      """
+//        |1 2
+//        |4 1
+//        |2 3
+//        |3 1
+//      """.stripMargin
+//    val testable = new Acyclicity(4, 4, graph)
+//    val res = testable.explorerAll()
+//    assert(res == 1, "Must be 1")
+//  }
+
+}
