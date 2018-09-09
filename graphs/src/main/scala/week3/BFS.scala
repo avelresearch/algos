059e79582fc42f03d468e702c47ed9adb68d0475
Pavel <pavel.oliynyk@gmail.com>
2018-09-09 12:03:30 +0100

    Update BfsSpec

--- graphs/src/main/scala/week3/BFS.scala
+++ graphs/src/main/scala/week3/BFS.scala
@@ -10,6 +10,35 @@ import scala.collection.mutable.ListBuffer
   is a flight between the corresponding two cities.
   Then, it suffices to find a shortest path from one of the given cities to the other one.
  */
+
+class BFS(n: Int, a: Array[List[Int]] ) {
+
+  def distance( s: Int, t: Int): Int = {
+    val dist = Array.fill(n)(Int.MaxValue)
+    dist.update(s, 0)
+
+    def distAcc(q: ListBuffer[Int]): Unit = q match {
+      case a if (a.isEmpty) => Unit
+      case _ => {
+        val u = q.head
+        q -= u
+        a(u).foreach(v => {
+          if (dist(v) == Int.MaxValue) {
+            q += v
+            dist.update(v, dist(u) + 1)
+          }
+        });
+        distAcc(q)
+      }
+    }
+
+    val queue: ListBuffer[Int] = ListBuffer(s)
+    distAcc(queue)
+
+    if (dist(t) == Int.MaxValue) -1 else dist(t)
+  }
+}
+
 object BFS {
 
   class InputReader {
@@ -29,39 +58,13 @@ object BFS {
   }
 
   def main(args: Array[String]): Unit = {
-
     val reader = new InputReader()
     val (n, m) = reader.init()
     val adj: Array[ListBuffer[Int]] = reader.getGraph(n, m)
-    val (x, y) = (reader.scanner.nextInt() - 1, reader.scanner.nextInt() - 1)
-
-    def distance(a: Array[ListBuffer[Int]], s: Int, t: Int): Int = {
-      val dist = Array.fill(n)(Int.MaxValue)
-      dist.update(s, 0)
-
-      def distAcc(q: ListBuffer[Int]): Unit = q match {
-        case a if (a.isEmpty) => Unit
-        case _ => {
-          val u = q.head
-          q -= u
-          a(u).foreach(v => {
-            if (dist(v) == Int.MaxValue) {
-              q += v
-              dist.update(v, dist(u) + 1)
-            }
-          });
-          distAcc(q)
-        }
-      }
-
-      val queue: ListBuffer[Int] = ListBuffer(s)
-      distAcc(queue)
-
-      if (dist(t) == Int.MaxValue) -1 else dist(t)
-    }
-
-    val d = distance(adj, x, y)
-    println(d)
+    val (s, t) = (reader.scanner.nextInt() - 1, reader.scanner.nextInt() - 1)
+    val b = new BFS(n, adj.map(_.toList) )
+    val res = b.distance(s, t)
+    println(res)
   }
 
 }
--- graphs/src/test/scala/week3/BfsSpec.scala
+++ graphs/src/test/scala/week3/BfsSpec.scala
@@ -1,19 +1,42 @@
 import org.scalatest.FlatSpec
+import scala.collection.mutable.ListBuffer
 
 class BfsSpec extends FlatSpec {
 
-  //TODO: add test coverage for Bfs task
-//  "Directed graph" should "be consistent" in {
-//    val graph: Array[List[Int]] =
-//      """
-//        |1 2
-//        |4 1
-//        |2 3
-//        |3 1
-//      """.stripMargin
-//    val testable = new Acyclicity(4, 4, graph)
-//    val res = testable.explorerAll()
-//    assert(res == 1, "Must be 1")
-//  }
+  trait Fixture {
+    def toGraph(m: Int, str: String) : Array[List[Int]] = {
+      val data = str.stripMargin
+        .split("\n")
+        .map(_.trim())
+        .filter(_.nonEmpty)
+        .map(_.split(" "))
+        .map(a => a.map(_.toInt))
+
+      val g: Array[ListBuffer[Int]] = Array.fill(m)(ListBuffer[Int]())
+      data.foreach(p => {
+        val (x, y) = (p(0), p(1))
+        g(x - 1).append(y - 1)
+        g(y - 1).append(x - 1)
+      })
+      g.map(_.toList)
+    }
+  }
+
+
+  "Distance between 2 and 4" should "be 2" in new Fixture {
+
+    val data =
+      """
+        |1 2
+        |4 1
+        |2 3
+        |3 1
+      """.stripMargin
+    val g = this.toGraph(4, data)
+
+    val testable = new BFS(4, g)
+    val res = testable.distance(1, 3)
+    assert(res == 2, "Number of edges in the path")
+  }
 
 }
